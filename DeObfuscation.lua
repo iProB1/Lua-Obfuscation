530190bdc1e336357503304365d1d72c612084d8
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-07-12 13:56:12 +0300

    typo

--- DeObfuscation.lua
+++ DeObfuscation.lua
@@ -1,4 +1,4 @@
---thanks to as06xz
+--thanks to as06zx aka aname
 
 local str  = [[c-oWpDNPJ!ketlRCB=/U!NS2(5ypT38s!d+42)bqBbdv=0wmZN92guO7#LI;7#P)U8W?.;Vk}S0MVWae!i-RTj11TS07DK4ZU]] -- Encoded func
 local func = decodeFunction(str)
